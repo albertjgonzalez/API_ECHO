51b9471f7f1a22d51d44db9caf4ceec1294ed035
albertjgonzalez <ajgonzalez1114@gmail.com>
2023-07-28 17:27:26 -0400

    looks a little better, works a little better. again..

--- app/gui_components.py
+++ app/gui_components.py
@@ -1,5 +1,4 @@
 import tkinter as tk
-from tkinter import ttk
 import urllib.request
 
 
@@ -11,13 +10,23 @@ def check_endpoint(url):
         return False
 
 
+def get_status_label_text(is_online):
+    return "Online" if is_online else "Offline"
+
+
 def on_client_id_selected(selected_client_id, selected_environment, clients, url_entry, isapi_entry, b2b_entry,
                           b2c_entry, muramasa_entry, url_status, isapi_status, b2b_status, b2c_status, muramasa_status):
-    # After selecting a client, set the environment to 'dev' if not already selected
+    # Clear the status labels
+    url_status.config(text="")
+    isapi_status.config(text="")
+    b2b_status.config(text="")
+    b2c_status.config(text="")
+    muramasa_status.config(text="")
+
     if not selected_environment.get():
         selected_environment.set('dev')
 
-    # Populate the input fields, but don't set the status yet.
+    # Rest of the function remains unchanged
     selected_id = selected_client_id.get()
     selected_client = next(client for client in clients if client.id == selected_id)
     env_data = selected_client.__dict__[selected_environment.get()]
@@ -28,35 +37,51 @@ def on_client_id_selected(selected_client_id, selected_environment, clients, url
     b2c_entry.delete(0, tk.END)
     muramasa_entry.delete(0, tk.END)
 
-    url_entry.insert(0, env_data.get("b2b_url", ""))
-    isapi_entry.insert(0, env_data.get("b2b_isapi", ""))
+    url_entry.insert(0, env_data.get("b2c_url", ""))
+    isapi_entry.insert(0, env_data.get("b2c_isapi", ""))
     b2b_entry.insert(0, env_data.get("b2b_url", ""))
-    b2c_entry.insert(0, env_data.get("b2c_url", ""))
+    b2c_entry.insert(0, env_data.get("b2b_isapi", ""))
     muramasa_entry.insert(0, env_data.get("muramasa", ""))
 
 
-def submit(url_entry, isapi_entry, b2b_entry, b2c_entry, muramasa_entry, url_status, isapi_status, b2b_status,
-           b2c_status, muramasa_status):
+def submit(url_entry, isapi_entry, b2b_entry, b2c_entry, muramasa_entry, url_status, isapi_status, b2b_status, b2c_status, muramasa_status):
     url = url_entry.get()
     isapi = isapi_entry.get()
     b2b = b2b_entry.get()
     b2c = b2c_entry.get()
     muramasa = muramasa_entry.get()
-
-    # Set status to "Loading..." while checking endpoints
-    url_status.config(text="Loading...")
-    isapi_status.config(text="Loading...")
-    b2b_status.config(text="Loading...")
-    b2c_status.config(text="Loading...")
-    muramasa_status.config(text="Loading...")
-
-    # Use after method to delay the submit function call and simulate loading
-    url_entry.after(100, lambda: check_and_update_endpoint(url, url_status))
-    isapi_entry.after(100, lambda: check_and_update_endpoint(isapi, isapi_status))
-    b2b_entry.after(100, lambda: check_and_update_endpoint(b2b, b2b_status))
-    b2c_entry.after(100, lambda: check_and_update_endpoint(b2c, b2c_status))
-    muramasa_entry.after(100, lambda: check_and_update_endpoint(muramasa, muramasa_status))
+    url_status.config(text=get_status_label_text(check_endpoint(url)))
+    isapi_status.config(text=get_status_label_text(check_endpoint(isapi)))
+    b2b_status.config(text=get_status_label_text(check_endpoint(b2b)))
+    b2c_status.config(text=get_status_label_text(check_endpoint(b2c)))
+    muramasa_status.config(text=get_status_label_text(check_endpoint(muramasa)))
 
 
-def check_and_update_endpoint(url, status_label):
-    status_label.config(text="Online" if check_endpoint(url) else "Offline")
+def on_environment_selected(selected_client_id, selected_environment, clients, url_entry, isapi_entry, b2b_entry,
+                            b2c_entry, muramasa_entry, url_status, isapi_status, b2b_status, b2c_status, muramasa_status):
+    # Clear the status labels
+    url_status.config(text="")
+    isapi_status.config(text="")
+    b2b_status.config(text="")
+    b2c_status.config(text="")
+    muramasa_status.config(text="")
+
+    if not selected_environment.get():
+        selected_environment.set('dev')
+
+    # Rest of the function remains unchanged
+    selected_id = selected_client_id.get()
+    selected_client = next(client for client in clients if client.id == selected_id)
+    env_data = selected_client.__dict__[selected_environment.get()]
+
+    url_entry.delete(0, tk.END)
+    isapi_entry.delete(0, tk.END)
+    b2b_entry.delete(0, tk.END)
+    b2c_entry.delete(0, tk.END)
+    muramasa_entry.delete(0, tk.END)
+
+    url_entry.insert(0, env_data.get("b2c_url", ""))
+    isapi_entry.insert(0, env_data.get("b2c_isapi", ""))
+    b2b_entry.insert(0, env_data.get("b2b_url", ""))
+    b2c_entry.insert(0, env_data.get("b2b_isapi", ""))
+    muramasa_entry.insert(0, env_data.get("muramasa", ""))
--- app/main.py
+++ app/main.py
@@ -1,7 +1,7 @@
 import tkinter as tk
 from tkinter import ttk
 from data_handler import Client, load_clients
-from gui_components import check_endpoint, on_client_id_selected, submit
+from gui_components import check_endpoint, on_client_id_selected, submit, on_environment_selected
 
 
 def fetch_status(url_entry, isapi_entry, b2b_entry, b2c_entry, muramasa_entry, url_status, isapi_status, b2b_status, b2c_status, muramasa_status):
@@ -10,11 +10,12 @@ def fetch_status(url_entry, isapi_entry, b2b_entry, b2c_entry, muramasa_entry, u
     b2b = b2b_entry.get()
     b2c = b2c_entry.get()
     muramasa = muramasa_entry.get()
-    url_status.config(text=str(check_endpoint(url)))
-    isapi_status.config(text=str(check_endpoint(isapi)))
-    b2b_status.config(text=str(check_endpoint(b2b)))
-    b2c_status.config(text=str(check_endpoint(b2c)))
-    muramasa_status.config(text=str(check_endpoint(muramasa)))
+    url_status.config(text="Loading...")
+    isapi_status.config(text="Loading...")
+    b2b_status.config(text="Loading...")
+    b2c_status.config(text="Loading...")
+    muramasa_status.config(text="Loading...")
+    url_entry.after(100, lambda: submit(url_entry, isapi_entry, b2b_entry, b2c_entry, muramasa_entry, url_status, isapi_status, b2b_status, b2c_status, muramasa_status))
 
 
 def save_last_selected_client(client_id):
@@ -30,12 +31,11 @@ def get_last_selected_client():
         return None
 
 
-
 clients = load_clients()
 clients_ids = [client.id for client in clients]
 
 app = tk.Tk()
-app.title("API Echo")
+app.title("Web Status")
 
 frame = ttk.Frame(app, padding="10")
 frame.grid(row=0, column=0, sticky=(tk.W, tk.E, tk.N, tk.S))
@@ -51,49 +51,52 @@ client_id_dropdown.grid(column=1, row=0, pady=5, sticky=tk.W)
 environment_label = ttk.Label(frame, text="Environment:")
 environment_label.grid(column=0, row=1, sticky=tk.W, pady=5)
 environment = tk.StringVar()
+environment.set("")  # Start with no environment selected
 environment_dropdown = ttk.Combobox(frame, textvariable=environment, values=["dev", "preprod", "prod"])
+environment_dropdown.bind("<<ComboboxSelected>>", lambda event: on_environment_selected(client_id, environment, clients, url_entry, isapi_entry, b2b_entry, b2c_entry, muramasa_entry, url_status, isapi_status, b2b_status, b2c_status, muramasa_status))
 environment_dropdown.grid(column=1, row=1, pady=5, sticky=tk.W)
 
-url_label = ttk.Label(frame, text="B2B URL:")
+url_label = ttk.Label(frame, text="B2C URL:")
 url_label.grid(column=0, row=2, sticky=tk.W, pady=5)
 url_entry = ttk.Entry(frame, width=50)
 url_entry.grid(column=1, row=2, pady=5, sticky=tk.W)
 
-isapi_label = ttk.Label(frame, text="B2B ISAPI:")
+url_status = ttk.Label(frame, text="")
+url_status.grid(column=2, row=2, sticky=tk.W, pady=5)
+
+isapi_label = ttk.Label(frame, text="B2C ISAPI:")
 isapi_label.grid(column=0, row=3, sticky=tk.W, pady=5)
 isapi_entry = ttk.Entry(frame, width=50)
 isapi_entry.grid(column=1, row=3, pady=5, sticky=tk.W)
 
-b2b_label = ttk.Label(frame, text="B2B Status:")
+isapi_status = ttk.Label(frame, text="")
+isapi_status.grid(column=2, row=3, sticky=tk.W, pady=5)
+
+b2b_label = ttk.Label(frame, text="B2B URL:")
 b2b_label.grid(column=0, row=4, sticky=tk.W, pady=5)
 b2b_entry = ttk.Entry(frame, width=50)
 b2b_entry.grid(column=1, row=4, pady=5, sticky=tk.W)
 
-b2c_label = ttk.Label(frame, text="B2C URL:")
+b2b_status = ttk.Label(frame, text="")
+b2b_status.grid(column=2, row=4, sticky=tk.W, pady=5)
+
+b2c_label = ttk.Label(frame, text="B2B ISAPI:")
 b2c_label.grid(column=0, row=5, sticky=tk.W, pady=5)
 b2c_entry = ttk.Entry(frame, width=50)
 b2c_entry.grid(column=1, row=5, pady=5, sticky=tk.W)
+
+b2c_status = ttk.Label(frame, text="")
+b2c_status.grid(column=2, row=5, sticky=tk.W, pady=5)
 
 muramasa_label = ttk.Label(frame, text="Muramasa:")
 muramasa_label.grid(column=0, row=6, sticky=tk.W, pady=5)
 muramasa_entry = ttk.Entry(frame, width=50)
 muramasa_entry.grid(column=1, row=6, pady=5, sticky=tk.W)
 
-submit_button = ttk.Button(frame, text="Check Status", command=lambda: app.after(100, submit, url_entry, isapi_entry, b2b_entry, b2c_entry, muramasa_entry, url_status, isapi_status, b2b_status, b2c_status, muramasa_status))
-submit_button.grid(column=1, row=7, pady=10, sticky=tk.W)
-
-url_status = ttk.Label(frame, text="")
-url_status.grid(column=2, row=2, sticky=tk.W, pady=5)
-isapi_status = ttk.Label(frame, text="")
-isapi_status.grid(column=2, row=3, sticky=tk.W, pady=5)
-b2b_status = ttk.Label(frame, text="")
-b2b_status.grid(column=2, row=4, sticky=tk.W, pady=5)
-b2c_status = ttk.Label(frame, text="")
-b2c_status.grid(column=2, row=5, sticky=tk.W, pady=5)
 muramasa_status = ttk.Label(frame, text="")
 muramasa_status.grid(column=2, row=6, sticky=tk.W, pady=5)
 
-client_id_dropdown.bind("<<ComboboxSelected>>", lambda event: on_client_id_selected(client_id, environment, clients, url_entry, isapi_entry, b2b_entry, b2c_entry, muramasa_entry, url_status, isapi_status, b2b_status, b2c_status, muramasa_status))
-environment_dropdown.bind("<<ComboboxSelected>>", lambda event: on_client_id_selected(client_id, environment, clients, url_entry, isapi_entry, b2b_entry, b2c_entry, muramasa_entry, url_status, isapi_status, b2b_status, b2c_status, muramasa_status))
+submit_button = ttk.Button(frame, text="Check Status", command=lambda: fetch_status(url_entry, isapi_entry, b2b_entry, b2c_entry, muramasa_entry, url_status, isapi_status, b2b_status, b2c_status, muramasa_status))
+submit_button.grid(column=1, row=7, pady=10, sticky=tk.W)
 
 app.mainloop()
